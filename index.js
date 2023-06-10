674cddac440053acc7c11bccd59661207b8a8ca0
babiloof <babiloof@gmail.com>
2023-06-10 13:18:54 +0200

    added commands

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-require('dotenv').config()
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 //channels...
 var logchannel = {};
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -15,6 +35,25 @@ client.once(Events.ClientReady, c => {
     client.guilds.cache.forEach( guild => {
 		logchannel[guild.id] = guild.channels.cache.find(x => x.name === '📇log');
 	});
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.on(Events.VoiceStateUpdate, (oldState, newState) => {
@@ -35,4 +74,4 @@ client.on(Events.VoiceStateUpdate, (oldState, newState) => {
 });
 
 // Log in to Discord with your client's token
-client.login(process.env.BOT_TOKEN);
+client.login(token);
